72a9ecf09b7ae6028ff9dd7e5ad1244c70bad1b2
cvgalagan <cvgalagan@gmail.com>
2020-10-21 19:24:19 +0300

    initial commit

---
+++ precache-manifest.88e265adc8a8960a6c3290335f8cac28.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc7cf69ef93503a92f0fa53baf180f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a268c137a664c1678f2e",
+    "url": "/static/css/main.5ef67bfc.chunk.css"
+  },
+  {
+    "revision": "c9151440ecff805d7ac6",
+    "url": "/static/js/2.618c73d2.chunk.js"
+  },
+  {
+    "revision": "807c71cdcc95c32660a45b4eeb32cb03",
+    "url": "/static/js/2.618c73d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a268c137a664c1678f2e",
+    "url": "/static/js/main.b4536131.chunk.js"
+  },
+  {
+    "revision": "ac8f00bf5896bbe51c57",
+    "url": "/static/js/runtime-main.1f15c76b.js"
+  },
+  {
+    "revision": "5b1b8b856d7a8cb1cb0bae6d0573f2e9",
+    "url": "/static/media/Lato-Bold.5b1b8b85.ttf"
+  },
+  {
+    "revision": "a2fb219c999a8fa6b95ad7c24890072e",
+    "url": "/static/media/Lato-Bold.a2fb219c.eot"
+  },
+  {
+    "revision": "cccb897485813c7c256901dbca54ecf2",
+    "url": "/static/media/Lato-Bold.cccb8974.woff2"
+  },
+  {
+    "revision": "d878b6c29b10beca227e9eef4246111b",
+    "url": "/static/media/Lato-Bold.d878b6c2.woff"
+  },
+  {
+    "revision": "4afee4c98483c85a33460792d1f0f6db",
+    "url": "/static/media/Lato-Light.4afee4c9.eot"
+  },
+  {
+    "revision": "7244318390cc4d36aac4a613ff42d308",
+    "url": "/static/media/Lato-Light.72443183.woff2"
+  },
+  {
+    "revision": "90301aa07d780a09812229d6375c3b28",
+    "url": "/static/media/Lato-Light.90301aa0.woff"
+  },
+  {
+    "revision": "cf44fd55d7045a2378f92f86ddfca3d3",
+    "url": "/static/media/Lato-Light.cf44fd55.ttf"
+  },
+  {
+    "revision": "27bd77b9162d388cb8d4c4217c7c5e2a",
+    "url": "/static/media/Lato-Regular.27bd77b9.woff"
+  },
+  {
+    "revision": "6d4e78225df0cfd5fe1bf3e8547fefe4",
+    "url": "/static/media/Lato-Regular.6d4e7822.ttf"
+  },
+  {
+    "revision": "8ab18d934cfa1e51dc8273cd8585387e",
+    "url": "/static/media/Lato-Regular.8ab18d93.eot"
+  },
+  {
+    "revision": "bd03a2cc277bbbc338d464e679fe9942",
+    "url": "/static/media/Lato-Regular.bd03a2cc.woff2"
+  }
+]);
